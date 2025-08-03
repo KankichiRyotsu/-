e0703c9ce685f7158e5052171b14bd7aae22f81e
KankichiRyotsu <tannyuu116@gmail.com>
2025-08-04 02:49:20 +0900

    Add files via upload

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
-python dump2VDOS.py --dump_file dump.sio2_300K --correlation_length 1000 --n_time_origins 50
+python dump2VDOS_2.py --dump_file dump.sio2_300K --correlation_length 1000 --n_time_origins 100 --mass_map_file mass.in --sim_time_step 0.5e-15
 
